d930b6637f763793630a2e0f8b535847977d85ad
zwq-qianyu <1347638091@qq.com>
2018-12-09 18:17:46 +0800

    完成所有页面favcon+完成教师端加班和物料模块的对接

---
+++ teacher/js/materal-record.js
@@ -0,0 +1,126 @@
+window.onload = function(){
+  init_data();
+}
+
+var base_url = 'http://134.175.152.210:8084';
+
+// 初始化页面数据
+function init_data(){
+  // 刷新库存列表
+  getAllMaterial();
+  // 根据条件显示物料登记记录
+  getSelectedPurchase();
+}
+
+// 刷新库存列表
+function getAllMaterial(){
+  let material_class = new Array;
+  $.ajax({
+    type: 'post',
+    url: base_url + '/material/getAllMaterial',
+    datatype: 'json',
+    data: {},
+    success: function(data){
+      if(data.status === 0){
+        // console.log(data);
+        let data_arr = data.data;
+        let html = '';
+        for(let i=0; i<data_arr.length; i++){
+          html += '<tr><td>'+data_arr[i].clazz+'</td><td>'+data_arr[i].num+'</td></tr>';
+          material_class.push(data_arr[i].clazz);
+        }
+        $('#materal_record_list').html(html);
+        // console.log(material_class);
+        // 导入所有物料种类
+        let html2 = '';
+        for(let j=0; j<material_class.length; j++){
+          html2 += '<option>'+material_class[j]+'</option>';
+        }
+        $('#select_meterial').html(html2);
+        html2 = '<option>选择物料种类</option>' + html2;
+        $('#record_history_selset_meterail').html(html2);
+      }
+    }
+  });
+}
+
+// 派出物料
+function decrMaterialNum(){
+  let num = $('#distribute_num').val();
+  let clazz = $('#select_meterial').val();
+  let sid = $('#distribute_stu_id').val();
+  let sname = $('#distribute_stu_name').val();
+
+  $.ajax({
+    type: 'post',
+    url: base_url + '/material/decrMaterialNum',
+    datatype: 'json',
+    data: {
+      'num': num,
+      'clazz': clazz,
+      'sid': sid,
+      'sname': sname
+    },
+    success: function(data){
+      if(data.status === 0){
+        // console.log(data);
+        swal(
+          '派出成功',
+          '派出物料记录成功',
+          'success'
+        );
+        // 清空派出物料时填的内容
+        $('#distribute_num').val("");
+        $('#select_meterial').val("");
+        $('#distribute_stu_id').val("");
+        $('#distribute_stu_name').val("");
+        init_data();
+      }
+      else{
+        console.log(data);
+        swal(
+          '派出失败',
+          '物料数量不足',
+          'error'
+        );
+      }
+    }
+  });
+}
+
+// 根据条件显示物料登记记录【接口有问题！！！】
+function getSelectedPurchase(){
+  let start_time = $('#start_time').val();
+  let end_time = $('#end_time').val();
+  let clazz = $('#record_history_selset_meterail').val();
+  let sid = $('#stu_id').val();
+  let sname = $('#stu_name').val();
+
+  if(clazz === "选择物料种类"){
+    clazz = "";
+  }
+  $.ajax({
+    type: 'post',
+    url: base_url + '/material/getApplys',
+    datatype: 'json',
+    data: {
+      'clazz': clazz,
+      'sid': sid,
+      'sname': sname,
+      'startTime': start_time,
+      'endTime': end_time
+    },
+    success: function(data){
+      if(data.status === 0){
+        console.log(data);
+        let data_arr = data.data;
+        let html = '';
+        for(let i=0; i<data_arr.length; i++){
+          html += '<tr><td>'+data_arr[i].apply_time+'</td><td>'+data_arr[i].sname+'</td><td>'+data_arr[i].sid+'</td><td>'+data_arr[i].clazz+'</td><td>'+data_arr[i].num+'</td><td>'+data_arr[i].tid+'</td></tr>';
+        }
+        $('#record_history_body').html(html);
+        // console.log(material_class);
+      }
+    }
+  });
+}
--- teacher/js/meteral-buy.js
+++ teacher/js/meteral-buy.js
@@ -6,12 +6,14 @@ var base_url = 'http://134.175.152.210:8084';
 
 // 初始化页面数据
 function init_data(){
+  // 刷新库存列表
   getAllMaterial();
 }
 
 // 刷新库存列表
 function getAllMaterial(){
   let material_class = new Array;
+  $('#new_semester').val("");
   $.ajax({
     type: 'post',
     url: base_url + '/material/getAllMaterial',
@@ -24,7 +26,7 @@ function getAllMaterial(){
         let html = '';
         for(let i=0; i<data_arr.length; i++){
           html += '<tr><td>'+data_arr[i].clazz+'</td><td>'+data_arr[i].num+'</td>';
-          html += '<td><button class="btn btn-danger btn-sm">删除</button></td></tr>';
+          html += '<td><button class="btn btn-danger btn-sm" id="'+data_arr[i].clazz+'" onclick="deleteOneMateral(this)">删除</button></td></tr>';
           material_class.push(data_arr[i].clazz);
         }
         $('#materal_buy_list').html(html);
@@ -37,11 +39,11 @@ function getAllMaterial(){
         $('#select_meterial').html(html2);
         $('#buy_history_selset_meterail').html(html2);
       }
-    }
+    },
   });
 }
 
-// 申购物料【接口有问题！！！】
+// 申购物料
 function addPurchase(){
   let num = $('#meteral_buy_num').val();
   let clazz = $('#select_meterial').val();
@@ -50,10 +52,15 @@ function addPurchase(){
     type: 'post',
     url: base_url + '/purchase/addPurchase',
     datatype: 'json',
+    // contentType: 'application/json;charset=UTF-8',
     data: {
       'num': num,
       'clazz': clazz,
     },
+    beforeSend: function(xhr) {
+      xhr.withCredentials = true;
+    },
+    crossDomain:true,
     success: function(data){
       console.log(data);
       if(data.status === 0){
@@ -73,30 +80,97 @@ function addPurchase(){
           'error'
         );
       }
+    },
+    error: function(data){
+      console.log(data);
     }
   });
 }
 
 // 根据条件显示物料申购记录【接口有问题！！！】
 function getSelectedPurchase(){
+  let clazz = $('#buy_history_selset_meterail').val();
+  let tname = $('#buy_history_selset_person').val();
+  let startTime = $('#star_time').val();
+  let endTime = $('#end_time').val();
+  if(tname === "申购人"){
+    tname = "";
+  }
   $.ajax({
     type: 'post',
     url: base_url + '/purchase/getSelectedPurchase',
     datatype: 'json',
     data: {
-
+      'clazz': clazz,
+      'tname': tname,
+      'startTime': startTime,
+      'endTime': endTime
     },
+    beforeSend: function(xhr) {
+      xhr.withCredentials = true;
+    },
+    crossDomain:true,
     success: function(data){
+      console.log(data);
       if(data.status === 0){
         console.log(data);
         let data_arr = data.data;
         let html = '';
         for(let i=0; i<data_arr.length; i++){
-          html += '<tr><td>'+data_arr[i].pur_time+'</td><td>'+data_arr[i].tname+'</td><td>学号</td><td>铝棒(根x2m)</td><td>200</td><td>力宏</td></tr>';
+          html += '<tr><td>'+data_arr[i].pur_time+'</td><td>'+data_arr[i].tname+'</td><td>'+data_arr[i].clazz+'</td><td>'+data_arr[i].num+'</td>';
         }
-        $('#record_history_body').html(html);
+        $('#buy_history_body').html(html);
         // console.log(material_class);
       }
+    }
+  });
+}
+
+// 删除一种物料【还没有接口】
+function deleteOneMateral(obj){
+  let clazz = obj.getAttribute('id');
+  console.log(clazz);
+
+}
+
+// 添加一种新的物料
+function addOneMateral(){
+  let new_material = $('#new_semester').val();
+  $.ajax({
+    type: 'post',
+    url: base_url + '/purchase/addPurchase',
+    datatype: 'json',
+    // contentType: 'application/json;charset=UTF-8',
+    data: {
+      'num': "0",
+      'clazz': new_material,
+    },
+    beforeSend: function(xhr) {
+      xhr.withCredentials = true;
+    },
+    crossDomain:true,
+    success: function(data){
+      console.log(data);
+      if(data.status === 0){
+        console.log(data);
+        swal(
+          '添加成功',
+          '添加新物料成功',
+          'success'
+        );
+        init_data();
+      }
+      else{
+        console.log(data);
+        swal(
+          '添加失败',
+          '添加新物料失败，请重试！',
+          'error'
+        );
+      }
+    },
+    error: function(data){
+      console.log(data);
     }
   });
 }
