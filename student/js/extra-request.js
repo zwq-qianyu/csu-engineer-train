17435e36e819992e482893c53c6196754d46509a
zwq-qianyu <1347638091@qq.com>
2018-12-13 17:12:00 +0800

    。

--- student/js/extra-request.js
+++ student/js/extra-request.js
@@ -111,3 +111,36 @@ function getMyOverworkApply(){
     }
   });
 }
+
+// 格林威治时间的转换
+Date.prototype.format = function(format){
+	var o = {
+            "M+" : this.getMonth()+1, //month
+            "d+" : this.getDate(), //day
+            "h+" : this.getHours(), //hour
+            "m+" : this.getMinutes(), //minute
+            "s+" : this.getSeconds(), //second
+            "q+" : Math.floor((this.getMonth()+3)/3), //quarter
+            "S" : this.getMilliseconds() //millisecond
+        }
+    if(/(y+)/.test(format))
+    format=format.replace(RegExp.$1,(this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    for(var k in o)
+    if(new RegExp("("+ k +")").test(format))
+    format = format.replace(RegExp.$1,RegExp.$1.length==1 ? o[k] : ("00"+ o[k]).substr((""+ o[k]).length));
+    return format;
+}
+// 获取标准时间格式
+function chGMT(gmtDate){
+	var mydate = new Date(gmtDate);
+	mydate.setHours(mydate.getHours() + 8);
+	// return mydate.format("yyyy-MM-dd hh:mm:ss");
+  return mydate.format("yyyy-MM-dd hh:mm");
+}
+// 获取小时
+function getGMThour(gmtDate){
+  var mydate = new Date(gmtDate);
+	mydate.setHours(mydate.getHours() + 8);
+	// return mydate.format("yyyy-MM-dd hh:mm:ss");
+  return Number(mydate.format("hh"));
+}
