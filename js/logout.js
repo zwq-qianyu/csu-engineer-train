675a6f1372e4a212e4669047fb02dde5b273b125
zwq-qianyu <1347638091@qq.com>
2018-12-30 16:21:51 +0800

    登出对接

---
+++ js/logout.js
@@ -0,0 +1,31 @@
+var base_url = 'http://134.175.152.210:8084';
+
+function logout(){
+  console.log("dfg");
+  $.ajax({
+    type: 'post',
+    // async: false,
+    url: base_url + '/logout',
+    datatype: 'json',
+    data: {},
+    // beforeSend: function(xhr) {
+    //   xhr.withCredentials = true;
+    // },
+    // crossDomain:true,
+    success: function(data){
+      // console.log(data);
+      if(data.status === 0){
+        console.log(data);
+        // window.location.href = './login.html';
+      }
+      else{
+        console.log(data);
+        swal(
+          '退出失败',
+          String(data.message),
+          'error'
+        );
+      }
+    },
+  });
+}
