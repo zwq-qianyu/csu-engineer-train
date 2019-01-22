fd29b810cacf9d20977bc56c8703bf8d8cb6ab27
YinAoXiong <yinaoxiong@gmail.com>
2019-01-22 18:39:11 +0800

    基本完成成绩列表编写

--- manager/js/api/api_common.js
+++ manager/js/api/api_common.js
@@ -3,12 +3,16 @@ var api_common={
     getAllBatch:function () {
         return post_query('/batch/getAllBatch');
     },
-    //根据批次名获取批次信息
-    getBatchByname:function (path) {
+    //根据批次名获取工序信息
+    getBatchProced:function (path) {
         return post_json('/proced/getBatchProced/' + path);
     },
     //获取所有的工序名
     getAllProced:function () {
         return post_query('/proced/getAllProced');
+    },
+    //根据批次获取对应的分组号
+    getAllSGroup(post_data){
+        return post_query('/batch/getAllSGroup',post_data);
     }
 };
---
+++ manager/js/config/config-socre.js
@@ -0,0 +1,6 @@
+//工序列表
+var processes = [];
+//成绩列表表头
+var score_list_columns=[];
+//成绩列表
+var score_list_data=[];
