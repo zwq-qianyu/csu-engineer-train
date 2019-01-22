3a3a037f0f189c92fded0107baa9266f3e3f1348
YinAoXiong <yinaoxiong@gmail.com>
2019-01-22 18:37:43 +0800

    调整weight api函数命名方式

--- manager/js/api/api_weight.js
+++ manager/js/api/api_weight.js
@@ -1,18 +1,18 @@
 var api_weight={
     //获取所有权重模板名
-    getAllTemplate:function () {
+    findAllTemplate:function () {
         return post_query('/proced/findAllTemplate');
     },
     //根据权重模板名获取内容
-    getTemplateItemByName:function (post_data) {
+    findTemplateItemByName:function (post_data) {
         return post_query('/proced/findTemplateItemByName',post_data);
     },
     //添加或修改权重模板
-    setTemplate:function (path,post_data) {
+    addTemplate:function (path, post_data) {
         return post_json('/proced/addTemplate?templateName='+path,post_data);
     },
     //绑定权重模板与批次
-    bindTemplateAndBatch:function (post_data) {
+    band:function (post_data) {
         return post_query('/proced/band',post_data);
     },
     //删除权重模板
