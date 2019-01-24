70732035473f5a255ab7d12cea29055bc11421eb
sheaton <819022816@qq.com>
2019-01-25 00:16:41 +0800

    物料申购

--- manager/js/api/api_material_purchase.js
+++ manager/js/api/api_material_purchase.js
@@ -96,5 +96,18 @@ var api_material_purchase = {
     },
     // 入库***************************************
     // 获取入库记录
+    getSaveBy5:function (postdata) {
+        return post_query(
+            '/save/getSaveBy5',
+            postdata
+        )
+    },
+    // 增加入库记录
+    addSave:function (postdata) {
+        return post_query(
+            '/save/add',
+            postdata
+        )
+    }
 
 }
